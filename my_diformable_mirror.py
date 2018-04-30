f52874126c7683bf2bb703feb2de31ddeb998a0c
Stephan Chang <33191624+ChangShuaibin@users.noreply.github.com>
2018-04-30 22:25:18 +0800

    Add files via upload

--- my_diformable_mirror.py
+++ my_diformable_mirror.py
@@ -1,39 +1,49 @@
 import ctypes
+import random
 from numpy.ctypeslib import ndpointer
-
-dm=None
-def loadDMDLL(dm_dll):
-    """
-    Handles loading the library (only once).
-    """
-    global dm
-    if dm is None:
-        dm = ctypes.cdll.LoadLibrary(dm_dll)
 
 class DMirror(object):
     dll_loaded=False
-    handle_grabbed=False
-    def __init__(self,dm_dll):
+    def __init__(self,dll_path):
         super().__init__()
-        self.status=0
+        self.status=ctypes.c_int(0)
+        self.dm=None
         if not DMirror.dll_loaded:
-            loadDMDLL(dm_dll)
+            self.loadDMDLL(dll_path)
             DMirror.dll_loaded = True
-            dm.mro_open.argtypes=[ndpointer(dtype=ctypes.c_int)]
-            dm.mro_close.argtypes=[ndpointer(dtype=ctypes.c_int)]
-            dm.mro_applySmoothCommand.argtypes=[ndpointer(dtype=ctypes.c_double),
+            self.dm.mro_open.argtypes=[ctypes.POINTER(ctypes.c_int)]
+            self.dm.mro_close.argtypes=[ctypes.POINTER(ctypes.c_int)]
+            self.dm.mro_applySmoothCommand.argtypes=[ndpointer(dtype=ctypes.c_double),
                                                 ctypes.c_char,
-                                                ndpointer(dtype=ctypes.c_int)]
+                                                     ctypes.POINTER(ctypes.c_int)]
+
+    def loadDMDLL(self,dll_path):
+        """
+        Handles loading the library (only once).
+        """
+        if self.dm is None:
+            self.dm = ctypes.cdll.LoadLibrary(dll_path)
 
     def DM_open(self):
-
         if DMirror.dll_loaded == True:
-            return dm.mro_open(ctypes.byref(self.status))
+            if self.dm.mro_open(ctypes.pointer(self.status))==0:
+                print('status: ',self.status)
 
     def DM_close(self):
         if DMirror.dll_loaded == True:
-            return dm.mro_close(ctypes.byref(self.status))
+            if self.dm.mro_close(ctypes.pointer(self.status))==0:
+                print('status: ',self.status)
 
-    def DM_applySmoothCommand(self,command,trig):
+    def DM_applySmoothCommand(self,command,trig='0'):
         if DMirror.dll_loaded == True:
-            return dm.mro_applySmoothCommand(command,trig,ctypes.byref(self.status))
+            if self.dm.mro_applySmoothCommand(command,trig,ctypes.pointer(self.status))==0:
+                print('status: ',self.status)
+
+if __name__=='__main__':
+    dm_handle=DMirror('dll_path')
+    dm_handle.DM_open()
+    command=[0]*52
+    for i in range(len(command)):
+        command[i]=random.random()/4
+    dm_handle.DM_applySmoothCommand(command)
+    dm_handle.DM_close()
